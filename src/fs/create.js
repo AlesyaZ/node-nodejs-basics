3535d55784dfb895ed8217231c9a9df4c467b2fa
alesyaz <alesya.zarembo@mail.ru>
2022-12-04 01:46:00 +0300

    feat: add solution fs/create

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,18 @@
+import fs from "fs";
+
+const path = "./src/fs/files/fresh.txt";
+
+const fileCreate = fs.createWriteStream(path);
+
 const create = async () => {
-    // Write your code here 
+  const fileText = "I am fresh and young";
+  const errorMessage = "FS operation failed";
+
+  fileCreate.write(fileText);
+
+  if (fs.existsSync(path)) {
+    console.log(errorMessage);
+  }
 };
 
 await create();
