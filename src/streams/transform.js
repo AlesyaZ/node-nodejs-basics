115e4acd691d19a6799addfa8e8cf880f7674c03
alesyaz <alesya.zarembo@mail.ru>
2022-12-05 13:41:34 +0300

    feat: add solution streams/transform

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,24 @@
+import { Transform, pipeline } from "stream";
+
+const { stderr, stdin, stdout } = process;
+
 const transform = async () => {
-    // Write your code here 
+  try {
+    const reverseTransform = new Transform({
+      transform(chunk, encoding, callback) {
+        const reverseString = chunk.toString().split("").reverse().join("");
+        this.push(reverseString);
+        console.log("\n");
+        callback();
+      },
+    });
+
+    pipeline(stdin, reverseTransform, stdout, (err) => {
+      stderr.write(err.message);
+    });
+  } catch (err) {
+    if (err) stderr.write(err.message);
+  }
 };
 
 await transform();
