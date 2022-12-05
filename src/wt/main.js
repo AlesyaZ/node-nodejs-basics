0abd5defacc325ce5925fefb1d37b3719be50d89
alesyaz <alesya.zarembo@mail.ru>
2022-12-05 18:28:40 +0300

    feat: add solution Worker Threads

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,34 @@
+import { cpus } from "os";
+import { Worker } from "worker_threads";
+
+const { stderr } = process;
+
+const path = "./src/wt/worker.js";
+
+let number = 10;
+
 const performCalculations = async () => {
-    // Write your code here
+  const resultPromise = [];
+
+  for (let index = 0; index < cpus().length; index++) {
+    const worker = new Worker(path, { workerData: index + number });
+
+    const creatWorker = new Promise((resolve, reject) => {
+      worker.on("message", (data) => resolve({ status: "resolved", data }));
+      worker.on("error", reject);
+    }).catch((err) => ({
+      status: "error",
+      data: null,
+    }));
+
+    resultPromise.push(creatWorker);
+  }
+
+  try {
+    await Promise.all(resultPromise).then((result) => console.log(result));
+  } catch (err) {
+    if (err) stderr.write(err.message);
+  }
 };
 
 await performCalculations();
